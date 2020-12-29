eaeceee5195c89c5d180afecff8890509c59feed
Neos21 <neos21@gmail.com>
2020-12-29 14:06:06 +0900

    Vercel

--- api/tv-films.js
+++ api/tv-films.js
@@ -1,13 +1,14 @@
 const tvFilms = require('../lib/index');
 
+// For Vercel
 module.exports = async (_req, res) => {
   try {
-    console.log(new Date().toISOString(), 'API : Start', result);
+    console.log(new Date().toISOString(), 'API : Start');
     const isUseProxy  = false;
     const isOutputLog = true;
     const result = await tvFilms(isUseProxy, isOutputLog);
     console.log(new Date().toISOString(), 'API Finished', result);
-    return res.send(200).json(result);
+    return res.status(200).json(result);
   }
   catch(error) {
     console.error(new Date().toISOString(), 'API : Failed', error);
